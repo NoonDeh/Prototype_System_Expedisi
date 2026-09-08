01bb96cdc8ed8570df375153d0b0ffbc5dc53a54
NoonD <rioivanders2@gmail.com>
2026-09-08 07:40:53 +0700

    Menambahkan fitur cek input

--- ongkir.py
+++ ongkir.py
@@ -6,19 +6,33 @@ print(f"- Reguler Ongkir = Rp10.000 + Rp5.000/5kg + Rp5.000/5km ")
 print(f"- Express Ongkir = Rp10.000 + Rp7.000/5kg + Rp7.000/5km ")
 print()
 
+def CekInputAngka(pesan):
+    while True:
+        try:
+            return float(input(pesan))
+        except ValueError:
+            print("Input tidak valid! Harap masukkan angka yang benar.")
 
 
-berat = float(input("Masukkan berat paket (kg): "))
-jarak = float(input("Jarak (km): "))
-pengiriman = str(input("jenis pengiriman \n1.reguler \n2.express \nPilih: "))
-pengiriman = pengiriman.lower()
+berat = CekInputAngka("Masukkan berat paket (kg): ")
+jarak = CekInputAngka("Jarak (km): ")
+while True:
+    try:
+        pengiriman = str(input("\njenis pengiriman \n1.reguler \n2.express \n\nPilih: "))
+        pengiriman = pengiriman.lower()
+        if pengiriman == "1" or pengiriman == "reguler":
+            pengiriman = "reguler"
+            break
+        elif pengiriman == "2" or pengiriman == "express":
+            pengiriman = "express"
+            break
+        else:
+            print("\n|| Masukkan input sesuai pilihan ||")
+    except ValueError:
+        print("Input tidak valid! Harap masukkan angka yang benar.")
 
-if pengiriman == "1":
-    pengiriman = "reguler"
-elif pengiriman == "2":
-    pengiriman = "express"
 
-if pengiriman == "reguler" or pengiriman == "express":
+if pengiriman == "reguler" or pengiriman == "express" or pengiriman == "2" or pengiriman == "1":
     if berat <= 0:
         print("Masukkan berat paket yang benar")
     else:
@@ -60,6 +74,7 @@ if pengiriman == "reguler" or pengiriman == "express":
 
             hitung_ongkir = 10000 + (ongkir * 7000)
 
+            print()
             print("================================")
             print("       HASIL TRANSAKSI          ") 
             print("================================")
@@ -71,3 +86,5 @@ if pengiriman == "reguler" or pengiriman == "express":
             print()
         else:
             print("Masukkan zona dengan benar")
+else:
+    print("Masukkan input dengan benar")
